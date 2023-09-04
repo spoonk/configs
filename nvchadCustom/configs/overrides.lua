8c58ab5e75f80e94a2e4046a147db041da92d34c
Lucas <cornwluc@cs.washington.edu>
2023-09-04 10:04:41 -0700

    add nvchad custom

---
+++ nvchadCustom/configs/overrides.lua
@@ -0,0 +1,67 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+local cmp = require "cmp"
+M.cmp = {
+  mapping = {
+    ["<C-j>"] = cmp.mapping.select_prev_item(),
+    ["<C-k>"] = cmp.mapping.select_next_item(),
+  },
+}
+
+return M
