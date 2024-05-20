9fff131ac12c1c683abd90224e3822f5d8fa31b6
spoonk <cornwlucas@gmail.com>
2024-05-20 08:35:01 -0700

    add poimandres

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -139,6 +139,20 @@ local plugins = {
       }
     end,
   },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup {
+        bold_vert_split = false,          -- use bold vertical separators
+        dim_nc_background = true,         -- dim 'non-current' window backgrounds
+        disable_background = true,        -- disable background
+        disable_float_background = false, -- disable background for floats
+        disable_italics = false,          -- disable italics
+      }
+    end,
+  },
 }
 
 return plugins
